21bcb58fe79065f8e6e4f2be4d3db4c6d5ccff43
Youngwoo Jin <duddnwkfgo7@gmail.com>
2024-03-31 16:48:53 -0700

    created asp.net empty for this project

--- STUDY.Console.ShiftLogger/STUDY.Console.ShiftLogger/obj/Debug/net8.0/STUDY.Console.ShiftLogger.AssemblyInfo.cs
+++ STUDY.Console.ShiftLogger/STUDY.Console.ShiftLogger/obj/Debug/net8.0/STUDY.Console.ShiftLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STUDY.Console.ShiftLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab4a083500186a1eeab15c3894b9bff10406f63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("STUDY.Console.ShiftLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STUDY.Console.ShiftLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
