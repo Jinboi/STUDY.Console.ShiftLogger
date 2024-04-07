b39bc9984815ec3260c38bfa7e0089e8b2d66122
Youngwoo Jin <duddnwkfgo7@gmail.com>
2024-04-07 16:59:49 -0700

    updated clockIn and clockOut

--- STUDY.ASP.ShiftLoggerTrySix/STUDY.ASP.ShiftLoggerTrySix/Program.cs
+++ STUDY.ASP.ShiftLoggerTrySix/STUDY.ASP.ShiftLoggerTrySix/Program.cs
@@ -1,28 +1,42 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
-app.MapGet("/person/{id:int}", (int id) =>
+var shifts = new List<Shift>();
+
+app.MapGet("/shifts", () => shifts);
+app.MapGet("/shifts/{id}", Results<Ok<Shift>,NotFound> (int id) =>
 {
-    return new PersonRecord(id, "Tim", "Corey");
+    var targetShift = shifts.SingleOrDefault(t => id == t.Id);
+    return targetShift is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetShift);
+});
+app.MapPost("/shifts", (Shift shift) =>
+{
+    shifts.Add(shift);
+    return TypedResults.Created("/shifts/{id}", shift);
 });
 
-app.MapPost("/person", (PersonRecord person) =>
+
+app.MapDelete("/shifts/{id}", (int id) =>
 {
-    return person;
+    shifts.RemoveAll(t => id == t.Id);
+    return TypedResults.NoContent();
 });
 
 app.Run();
-record PersonRecord(int Id, string FirstName, string LastName);
-
+public record Shift(int Id, int EmployeeId, DateTime ClockIn, DateTime ClockOut, bool IsDoneWorking);
